6c87746485d9283bf077c01e138059048720ee32
Andrey Kulakovskiy <kulakovskiy@gmail.com>
2014-03-23 00:14:37 +0400

    Little code cleaning and improvements...

--- sms_test.r
+++ sms_test.r
@@ -3,7 +3,7 @@
 require(RCurl)
     nodename = Sys.info()['nodename']
     time= Sys.time()
-msg.text = paste(time, nodename, 'Hello world!' sep = ' |')
+    msg.text = paste(time, nodename, 'Hello world, yohohohohoooo!' sep = ' |')
 
 api_id = 'dfdbb9ca-08dd-6954-cd3b-ee8556c9a9b5'
 
